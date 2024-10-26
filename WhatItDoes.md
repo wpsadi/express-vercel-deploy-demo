57e6b8c581062623044337d33cf8e45fd2ea4bc3
Aditya <adityaislegend@outlook.com>
2024-10-27 02:37:22 +0530

    successfull

---
+++ WhatItDoes.md
@@ -0,0 +1,6 @@
+All you need is create `vercel.json`
+and change the src value to the path `path of entry file` in my case _index.js_
+then second you need is 
+`export default app`
+
+rest is done
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const app = require("./app.js");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use((req,res,next)=>{
+    console.log(req.url)
+    console.log("Request received");
+    // console.log(req.query);
+    console.log(req.params );
+    next();
+})
+
+app.use("/hi/:name", (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+  });
+  
 
 
+app.use((req,res,next)=>{
+    res.status(404).send("Not found")
+})
 
-const port = process.env.PORT || 3000;
+app.listen(3001, () => console.log("Server ready on port 3000."));
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+
+export default app;
