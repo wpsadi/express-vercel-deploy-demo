ee0a0e4ad82a9faa0272f81756d5bdf9afe5688c
Aditya <adityaislegend@outlook.com>
2024-10-26 22:22:58 +0530

    save it

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use((req,res)=>{
+    res.status(200).text("Hello there this is being runned from vercel servrless function, and is and xpress server")
+})
+
+
+export default app
